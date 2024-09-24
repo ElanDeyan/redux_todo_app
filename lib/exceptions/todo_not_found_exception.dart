c507cac6ed8b939ec3d3740bf69236cee4bac4b1
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-24 19:57:36 -0300

    feat: adds an exception

---
+++ lib/exceptions/todo_not_found_exception.dart
@@ -0,0 +1,18 @@
+import 'package:redux_todo_app/models/models.dart';
+import 'package:redux_todo_app/store/app_store.dart';
+import 'package:redux_todo_app/store/store.dart';
+
+/// {@template todo_not_found_exception}
+/// An [Exception] class for missing [Todo]s in [AppStore]
+/// {@endtemplate}
+final class TodoNotFoundException implements Exception {
+  /// {@macro todo_not_found_exception}
+  const TodoNotFoundException(this.todo, [String? message])
+      : message = message ?? 'Todo not found: $todo';
+
+  /// [String] for a message of the exception
+  final String message;
+
+  /// The missing [Todo].
+  final Todo todo;
+}
