1742be31a49ee848c53448747c4b8013afd669f6
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-24 21:05:48 -0300

    feat: adds an empty home page and adds the AppStoreProvider

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// {@template home_page}
+/// The main page of the application
+/// {@endtemplate}
+final class HomePage extends StatelessWidget {
+  /// {@macro home_page}
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
