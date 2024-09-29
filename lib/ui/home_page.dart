6025b96a71541bc637ca1fd0f3bed6910b23a280
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-29 17:31:07 -0300

    feat: adds filters and refactors this SliverListView

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux_todo_app/app_state.dart';
-import 'package:redux_todo_app/models/models.dart';
 import 'package:redux_todo_app/ui/add_todo_floating_action_button.dart';
-import 'package:redux_todo_app/ui/todo_tile/todo_tile.dart';
+import 'package:redux_todo_app/ui/todos_list_view.dart';
 
 /// {@template home_page}
 /// The main page of the application
@@ -19,34 +16,7 @@ final class HomePage extends StatelessWidget {
         leading: const Icon(Icons.science_outlined),
         title: const Text('Redux todo app'),
       ),
-      body: CustomScrollView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        slivers: [
-          SliverToBoxAdapter(
-            child: Center(
-              child: Text(
-                'What to do now?',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 10,
-            ),
-          ),
-          StoreConnector<AppState, List<Todo>>(
-            converter: (store) => store.state.todos,
-            builder: (context, todos) => SliverList.builder(
-              itemCount: todos.length,
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.all(8),
-                child: TodoTile(todo: todos[index]),
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: const TodosListView(),
       floatingActionButton: const AddTodoFloatingActionButton(),
     );
   }
