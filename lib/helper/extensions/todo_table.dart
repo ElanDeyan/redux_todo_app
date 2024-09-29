d62ed077a64546c6847cd09d9263ee49ef2f9e78
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-28 22:23:40 -0300

    feat: adds helper method

---
+++ lib/helper/extensions/todo_table.dart
@@ -0,0 +1,15 @@
+import 'package:redux_todo_app/db/app_database.dart';
+import 'package:redux_todo_app/models/models.dart';
+
+/// Helper extensions for [TodoTable].
+extension TodoTableExtension on TodoTable {
+  /// "Factory" to creates a new [TodoTable] from a [Todo] model.
+  static TodoTable fromTodoModel(Todo todo) {
+    return TodoTable(
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      isCompleted: todo.isCompleted,
+    );
+  }
+}
