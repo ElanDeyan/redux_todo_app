71bc035aa3871ecd879b59b120b06c142fa037df
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-28 22:28:04 -0300

    feat: makes AppStoreProvider starting with database todos, adds databaseWriter middleware and loading progress indicator

--- lib/providers/app_store_provider.dart
+++ lib/providers/app_store_provider.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-import 'package:redux_todo_app/actions/actions.dart';
 import 'package:redux_todo_app/app_state.dart';
+import 'package:redux_todo_app/db/app_database.dart';
+import 'package:redux_todo_app/helper/extensions/todo_model.dart';
 import 'package:redux_todo_app/middlewares/middlewares.dart';
+import 'package:redux_todo_app/models/todo.dart';
 import 'package:redux_todo_app/reducers/reducers.dart';
+import 'package:redux_todo_app/service_locator.dart';
 
 /// {@template app_store_provider}
 /// [StoreProvider] for the app.
 /// {@endtemplate}
-final class AppStoreProvider extends StatelessWidget {
+final class AppStoreProvider extends StatefulWidget {
   /// {@macro app_store_provider}
   const AppStoreProvider({
     required this.child,
@@ -20,16 +23,46 @@ final class AppStoreProvider extends StatelessWidget {
   final Widget child;
 
   @override
+  State<AppStoreProvider> createState() => _AppStoreProviderState();
+}
+
+class _AppStoreProviderState extends State<AppStoreProvider> {
+  late final Future<List<Todo>> _initialItems;
+  @override
+  void initState() {
+    super.initState();
+    _initialItems = serviceLocator<AppDatabase>()
+        .all
+        .then((rows) => rows.map(TodoModel.fromTodoTable).toList());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StoreProvider(
-      store: Store<AppState>(
-        TypedReducer(todoReducer).call,
-        initialState: const AppState(),
-        middleware: [
-          TypedMiddleware<AppState, TodoActions>(stateTransitionLogger).call,
-        ],
-      ),
-      child: child,
+    return FutureBuilder<List<Todo>>(
+      future: _initialItems,
+      builder: (context, snapshot) {
+        final connectionState = snapshot.connectionState;
+        final hasError = snapshot.hasError;
+        final hasData = snapshot.hasData;
+
+        final data = snapshot.data;
+        return switch ((connectionState, hasError, hasData)) {
+          (ConnectionState.active || ConnectionState.done, _, true)
+              when data != null =>
+            StoreProvider(
+              store: Store<AppState>(
+                TypedReducer(todoReducer).call,
+                initialState: AppState(todos: data),
+                middleware: [
+                  TypedMiddleware(databaseWriter).call,
+                  TypedMiddleware(stateTransitionLogger).call,
+                ],
+              ),
+              child: widget.child,
+            ),
+          _ => const Scaffold(body: Center(child: CircularProgressIndicator())),
+        };
+      },
     );
   }
 }
