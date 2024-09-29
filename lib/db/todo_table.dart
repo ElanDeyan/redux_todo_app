302f7851a42372b946679f9547a6087b2987c8ef
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-28 22:51:26 -0300

    docs: adds docs for table fields

--- lib/db/todo_table.dart
+++ lib/db/todo_table.dart
@@ -1,11 +1,20 @@
 import 'package:drift/drift.dart';
+import 'package:redux_todo_app/db/app_database.dart';
 import 'package:redux_todo_app/models/models.dart';
 
 /// Drift's table for all [Todo]
 @DataClassName('TodoTable')
 class Todos extends Table {
+  /// [Todo]'s id. Can be null when adding and the [AppDatabase] will
+  /// give it.
   IntColumn get id => integer().autoIncrement().nullable()();
+
+  /// [Todo]'s title.
   TextColumn get title => text().withLength(min: 1)();
+
+  /// [Todo]'s description
   TextColumn get description => text().nullable()();
+
+  /// [Todo]'s completion.
   BoolColumn get isCompleted => boolean().withDefault(const Constant(false))();
 }
