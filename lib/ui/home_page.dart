1f4927d1c53d1aba59d87efc4d2f5840580503fc
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-28 22:52:16 -0300

    dev: adds basic home page for some testing

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux_todo_app/actions/actions.dart';
+import 'package:redux_todo_app/app_state.dart';
+import 'package:redux_todo_app/models/models.dart';
 
 /// {@template home_page}
 /// The main page of the application
@@ -9,6 +13,45 @@ final class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    const sample = Todo(title: 'A', description: 'Description');
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.science_outlined),
+        title: const Text('Redux todo app'),
+      ),
+      body: ListView(
+        children: [
+          const Text('Here are your tasks:'),
+          StoreConnector<AppState, List<Todo>>(
+            converter: (store) => store.state.todos,
+            builder: (context, value) => Text(value.toString()),
+          ),
+          StoreConnector<AppState, VoidCallback>(
+            converter: (store) => () => store
+                .dispatch(ToggleTaskCompletion(todo: store.state.todos.last)),
+            builder: (context, callback) => ElevatedButton(
+              onPressed: callback,
+              child: const Text('Toggle last task completion'),
+            ),
+          ),
+          StoreConnector<AppState, VoidCallback>(
+            converter: (store) =>
+                () => store.dispatch(RemoveTask(todo: store.state.todos.last)),
+            builder: (context, callback) => ElevatedButton(
+              onPressed: callback,
+              child: const Text('Remove last'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: StoreBuilder<AppState>(
+        builder: (context, store) => FloatingActionButton.extended(
+          onPressed: () => store.dispatch(const AddTask(todo: sample)),
+          label: const Text('Add task'),
+          icon: const Icon(Icons.add_outlined),
+        ),
+      ),
+    );
   }
 }
