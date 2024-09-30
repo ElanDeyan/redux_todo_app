7e84611cd033c16baade4ccdc34d695e093be099
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-30 19:36:39 -0300

    docs: adds docs

--- lib/ui/add_todo_form/add_todo_form_title.dart
+++ lib/ui/add_todo_form/add_todo_form_title.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:redux_todo_app/ui/add_todo_form/add_todo_form.dart';
 
+/// {@template add_todo_form_title}
+/// Widget to displays a title for the [AddTodoForm]
+/// {@endtemplate}
 class AddTodoFormTitle extends StatelessWidget {
+  /// {@macro add_todo_form_title}
   const AddTodoFormTitle() : super(key: const Key('add_todo_form_title'));
 
   @override
