d5a0317bb4477b1115390f98a0d0c45170f12f12
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-10-01 06:42:13 -0300

    refactor: extends equatable

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,14 +1,18 @@
+import 'package:equatable/equatable.dart';
 import 'package:redux_todo_app/models/models.dart';
 
 /// {@template app_store}
 /// App's store in redux architecture.
 /// {@endtemplate}
-final class AppState {
+final class AppState extends Equatable {
   /// {@macro app_store}
   const AppState({this.todos = const <Todo>[]});
 
   /// A list containing all [Todo]'s of the app.
   final List<Todo> todos;
+
+  @override
+  List<Object?> get props => [todos];
 
   @override
   String toString() {
